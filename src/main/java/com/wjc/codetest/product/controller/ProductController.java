38bb9caff1e2c0279eb378318324de21398b3c34
ksh-UL <kimsh520@ulalalab.com>
2026-01-08 14:49:38 +0900

    [chore] swagger 적용 및 RestFul 하게 변경

--- src/main/java/com/wjc/codetest/product/controller/ProductController.java
+++ src/main/java/com/wjc/codetest/product/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.wjc.codetest.product.model.domain.Product;
 import com.wjc.codetest.product.model.request.UpdateProductRequest;
 import com.wjc.codetest.product.model.response.ProductListResponse;
 import com.wjc.codetest.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -14,42 +16,49 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
 @RequiredArgsConstructor
+@RequestMapping("/api/product")
+@Tag(name = "Product API", description = "제품관련 API")
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping(value = "/get/product/by/{productId}")
+    @Operation(summary = "제품상세 조회", description = "제품 ID로 해당 제품을 조회한다.")
+    @GetMapping(value = "/{productId}")
     public ResponseEntity<Product> getProductById(@PathVariable(name = "productId") Long productId){
         Product product = productService.getProductById(productId);
         return ResponseEntity.ok(product);
     }
-
-    @PostMapping(value = "/create/product")
+    
+    @Operation(summary = "제품 등록", description = "새로운 제품을 등록한다.")
+    @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody CreateProductRequest dto){
         Product product = productService.create(dto);
         return ResponseEntity.ok(product);
     }
 
-    @PostMapping(value = "/delete/product/{productId}")
+    @Operation(summary = "제품 삭제", description = "해당 제품을 삭제한다.")
+    @DeleteMapping(value = "/{productId}")
     public ResponseEntity<Boolean> deleteProduct(@PathVariable(name = "productId") Long productId){
         productService.deleteById(productId);
         return ResponseEntity.ok(true);
     }
 
-    @PostMapping(value = "/update/product")
+    @Operation(summary = "제품 수정", description = "해당 제품을 수정한다.")
+    @PutMapping
     public ResponseEntity<Product> updateProduct(@RequestBody UpdateProductRequest dto){
         Product product = productService.update(dto);
         return ResponseEntity.ok(product);
     }
 
-    @PostMapping(value = "/product/list")
-    public ResponseEntity<ProductListResponse> getProductListByCategory(@RequestBody GetProductListRequest dto){
+    @Operation(summary = "제품 목록조회", description = "제품목록을 조회한다.")
+    @GetMapping
+    public ResponseEntity<ProductListResponse> getProductListByCategory(GetProductListRequest dto){
         Page<Product> productList = productService.getListByCategory(dto);
         return ResponseEntity.ok(new ProductListResponse(productList.getContent(), productList.getTotalPages(), productList.getTotalElements(), productList.getNumber()));
     }
 
-    @GetMapping(value = "/product/category/list")
+    @Operation(summary = "제품별 category 목록조회", description = "제품별 category 를 조회한다.")
+    @GetMapping(value = "/category/list")
     public ResponseEntity<List<String>> getProductListByCategory(){
         List<String> uniqueCategories = productService.getUniqueCategories();
         return ResponseEntity.ok(uniqueCategories);
